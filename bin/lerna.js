b4a1073e888047620a70130d62dd2a4e80f137b6
Joshua Nelson <jonelson@atlassian.com>
2016-05-25 19:17:13 +1000

    Merge correctly

--- bin/lerna.js
+++ bin/lerna.js
@@ -23,13 +23,9 @@ var cli = meow([
   "  --canary, -c         Publish packages after every successful merge using the sha as part of the tag",
   "  --skip-git           Skip commiting, tagging, and pushing git changes (only affects publish)",
   "  --npm-tag [tagname]  Publish packages with the specified npm dist-tag",
-<<<<<<< HEAD
+  "  --scope [glob]       Restricts the scope to package names matching the given glob (Works only in combination with the 'run' and the 'exec' command).",
   "  --force-publish      Force publish for the specified packages (comma-separated) or all packages using * (skips the git diff check for changed packages)",
   "  --concurrency        How many threads to use if lerna parallelises the tasks (defaults to 4)"
-=======
-  "  --scope [glob]       Restricts the scope to package names matching the given glob (Works only in combination with the 'run' and the 'exec' command).",
-  "  --force-publish      Force publish for the specified packages (comma-separated) or all packages using * (skips the git diff check for changed packages)"
->>>>>>> lerna-exec-lerna-run-and-restrict
 ], {
   alias: {
     independent: "i",
